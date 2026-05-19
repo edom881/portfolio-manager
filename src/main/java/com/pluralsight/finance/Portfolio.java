532206a00c783b3b9cb3ad3ca04ab39f19974578
edomg <edomgezahegn54@gmail.com>
2026-05-19 15:12:58 -0500

    Add fields and add method to Portfolio class

--- src/main/java/com/pluralsight/finance/Portfolio.java
+++ src/main/java/com/pluralsight/finance/Portfolio.java
@@ -5,4 +5,17 @@ import java.util.List;
 
 public class Portfolio {
 
+    private String name;
+    private String owner;
+    private List<Valuable> assets;
+
+    public Portfolio(String name, String owner) {
+        this.name = name;
+        this.owner = owner;
+        this.assets = new ArrayList<>();
+    }
+
+    public void add(Valuable asset) {
+        assets.add(asset);
+    }
 }
